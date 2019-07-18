285c98b7bd573de67614f0c43ef28ad0563ee7bb
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2019-07-17 21:34:48 -0700

    Create build scripts for Windows and *nix

---
+++ build_res/build.ps1
@@ -0,0 +1,7 @@
+rm ./minimal.js ./minimal.css ./minimal.min.js ./minimal.min.css
+cp ./build/minimal.css ./minimal.css
+cp ./src/minimal.js ./minimal.js
+npm update
+sass ./src/:./build/
+npx cleancss --level 2 ./minimal.css --output ./minimal.min.css
+npx uglifyjs ./minimal.js --compress --mangle --output ./minimal.min.js
