55b9b131ff1cf88615486531a228186fb5b77bc7
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2019-07-21 19:00:05 -0700

    Fix Windows build script

--- build_res/build.ps1
+++ build_res/build.ps1
@@ -1,7 +1,7 @@
 rm ./minimal.js ./minimal.css ./minimal.min.js ./minimal.min.css
+sass ./src/:./build/
 cp ./build/minimal.css ./minimal.css
 cp ./src/minimal.js ./minimal.js
 npm update
-sass ./src/:./build/
 npx cleancss --level 2 ./minimal.css --output ./minimal.min.css
 npx uglifyjs ./minimal.js --compress --mangle --output ./minimal.min.js
