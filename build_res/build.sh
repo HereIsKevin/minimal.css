d8382d8ace8c15800cadced1b100b1f5a2c1504d
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2019-07-22 21:16:31 -0700

    Update versions in build script

--- build_res/build.sh
+++ build_res/build.sh
@@ -52,13 +52,13 @@ release() {
     npx uglifyjs ./src/minimal.js --compress --mangle --output ./build/minimal.min.js
     echo "[80%-85%] Add copyright notices to files"
     mkdir ./out/
-    echo "/* minimal.css v0.6.0 CSS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.css
+    echo "/* minimal.css v0.6.2 CSS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.css
     cat ./build/minimal.min.css >> ./out/minimal.min.css
-    echo "/* minimal.css v0.6.0 JS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.js
+    echo "/* minimal.css v0.6.2 JS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.js
     cat ./build/minimal.min.js >> ./out/minimal.min.js
-    echo "/* minimal.css v0.6.0 CSS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.css
+    echo "/* minimal.css v0.6.2 CSS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.css
     cat ./build/minimal.css >> ./out/minimal.css
-    echo "/* minimal.css v0.6.0 JS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.js
+    echo "/* minimal.css v0.6.2 JS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.js
     cat ./src/minimal.js >> ./out/minimal.js
     echo "[85%-90%] Copy tests to final build"
     cp -r ./tests/ ./out/tests/
