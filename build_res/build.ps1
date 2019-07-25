6578788b6ef7bfcb7bc572fcf2c7e249d321dbe1
Kevin Feng <48870276+kevinfen@users.noreply.github.com>
2019-07-24 20:48:34 -0700

    Fix build.ps1 bugs

--- build_res/build.ps1
+++ build_res/build.ps1
@@ -1,16 +1,16 @@
 #!/usr/bin/env powershell
 
 function show_help() {
-    Write-Host "usage: build.ps1 [--help | -h] [--release | -r] [--clean | -c]  [--watch | -w] [--dev | -d]"
-    Write-Host ""
-    Write-Host "Build script for minimal.css for Windows, a direct port of build.sh"
-    Write-Host ""
-    Write-Host "options:"
-    Write-Host "    --help, -h          Show the help message and exit"
-    Write-Host "    --release, -r       Build in production ready, release mode"
-    Write-Host "    --clean, -c         Clean the build directory and build files"
-    Write-Host "    --watch, -w         Watch the files, good for developement"
-    Write-Host "    --dev, -d           Developement mode without file watching"
+    Write-Output "usage: build.ps1 [--help | -h] [--release | -r] [--clean | -c]  [--watch | -w] [--dev | -d]"
+    Write-Output ""
+    Write-Output "Build script for minimal.css for Windows, a direct port of build.sh"
+    Write-Output ""
+    Write-Output "options:"
+    Write-Output "    --help, -h          Show the help message and exit"
+    Write-Output "    --release, -r       Build in production ready, release mode"
+    Write-Output "    --clean, -c         Clean the build directory and build files"
+    Write-Output "    --watch, -w         Watch the files, good for developement"
+    Write-Output "    --dev, -d           Developement mode without file watching"
 }
 
 function clean() {
@@ -31,70 +31,70 @@ function clean() {
     }
 
     if (Test-Path ./build/) {
-        Remove-Item -recurse ./build/
+        Remove-Item -Recurse -Force ./build/
     }
 
     if (Test-Path ./out/) {
-        Remove-Item -Recurse ./out/
+        Remove-Item -Recurse -Force ./out/
     }
 }
 
 function release() {
-    Write-Host "=====Begin build for release====="
-    Write-Host "[0%-20%] Cleaning build directory"
+    Write-Output "=====Begin build for release====="
+    Write-Output "[0%-20%] Cleaning build directory"
     clean
-    Write-Host "[20%-40%] Update NPM dependencies"
+    Write-Output "[20%-40%] Update NPM dependencies"
     npm update
-    Write-Host "[40%-60%] Build SCSS with Sass"
+    Write-Output "[40%-60%] Build SCSS with Sass"
     npx sass ./src/:./build/
-    Write-Host "[60%-70%] Minify CSS"
+    Write-Output "[60%-70%] Minify CSS"
     npx cleancss --level 2 ./build/minimal.css --output ./build/minimal.min.css
-    Write-Host "[70%-80%] Minify and mangle JS"
+    Write-Output "[70%-80%] Minify and mangle JS"
     npx uglifyjs ./src/minimal.js --compress --mangle --output ./build/minimal.min.js
-    Write-Host "[80%-85%] Add copyright notices to files"
+    Write-Output "[80%-85%] Add copyright notices to files"
     New-Item -ItemType Directory ./out/ > $null
-    Write-Host "/* minimal.css v0.6.3 CSS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.css
+    Write-Output "/* minimal.css v0.6.3 CSS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.css
     Get-Content ./build/minimal.min.css >> ./out/minimal.min.css
-    Write-Host "/* minimal.css v0.6.3 JS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.js
+    Write-Output "/* minimal.css v0.6.3 JS component for production | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.min.js
     Get-Content ./build/minimal.min.js >> ./out/minimal.min.js
-    Write-Host "/* minimal.css v0.6.3 CSS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.css
+    Write-Output "/* minimal.css v0.6.3 CSS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.css
     Get-Content ./build/minimal.css >> ./out/minimal.css
-    Write-Host "/* minimal.css v0.6.3 JS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.js
+    Write-Output "/* minimal.css v0.6.3 JS component for tests | AGPLv3 | github.com/HereIsKevin/minimal.css */" > ./out/minimal.js
     Get-Content ./src/minimal.js >> ./out/minimal.js
-    Write-Host "[85%-90%] Copy tests to final build"
+    Write-Output "[85%-90%] Copy tests to final build"
     Copy-Item -Recurse ./tests/ ./out/tests/
-    Write-Host "[90%-95%] Copy final build README"
+    Write-Output "[90%-95%] Copy final build README"
     Copy-Item ./build_res/README.md ./out/README.md
-    Write-Host "[95%-100%] Copy LICENSE to build"
+    Write-Output "[95%-100%] Copy LICENSE to build"
     Copy-Item ./LICENSE ./out/LICENSE
 }
 
 function watch() {
-    Write-Host "=====Watching for developement====="
-    Write-Host "[0%-20%] Cleaning build directory"
+    Write-Output "=====Watching for developement====="
+    Write-Output "[0%-20%] Cleaning build directory"
     clean
-    Write-Host "[20%-40%] Update NPM dependencies"
+    Write-Output "[20%-40%] Update NPM dependencies"
     npm update
-    Write-Host "[40%-60%] Create symlinks to resources"
+    Write-Output "[40%-60%] Create symlinks to resources"
     sass ./src/:./build/
     New-Item -ItemType symboliclink ./minimal.css -Value ./build/minimal.css > $null
     New-Item -ItemType symboliclink ./minimal.js -Value ./src/minimal.js > $null
-    Write-Host "[60%-100%] Begin Sass watch compilation"
+    Write-Output "[60%-100%] Begin Sass watch compilation"
     sass --watch ./src/:./build/
 }
 
 function dev() {
-    Write-Host "=====Watching for developement====="
-    Write-Host "[0%-20%] Cleaning build directory"
+    Write-Output "=====Watching for developement====="
+    Write-Output "[0%-20%] Cleaning build directory"
     clean
-    Write-Host "[20%-40%] Update NPM dependencies"
+    Write-Output "[20%-40%] Update NPM dependencies"
     npm update
-    Write-Host "[40%-60%] Create symlinks to resources"
+    Write-Output "[40%-60%] Create symlinks to resources"
     New-Item -ItemType Directory ./build/ > $null
     New-Item -ItemType File ./build/minimal.css > $null
     New-Item -ItemType symboliclink ./minimal.css -Value ./build/minimal.css > $null
     New-Item -ItemType symboliclink ./minimal.js -Value ./src/minimal.js > $null
-    Write-Host "[60%-100%] Build SCSS with Sass"
+    Write-Output "[60%-100%] Build SCSS with Sass"
     sass ./src/:./build/
 }
 
@@ -102,7 +102,7 @@ function flag_parser() {
     if (($args[0] -eq '--help') -or ($args[0] -eq '-h') -or (-not $args)) {
         show_help
     } elseif (($args[0] -eq '--clean') -or ($args[0] -eq '-c')) {
-        Write-Host "=====Clean build directory====="
+        Write-Output "=====Clean build directory====="
         clean
     } elseif (($args[0] -eq '--release') -or ($args[0] -eq '-r')) {
         release
@@ -111,7 +111,7 @@ function flag_parser() {
     } elseif (($args[0] -eq '--dev') -or ($args[0] -eq '-d')) {
         dev
     } else {
-        Write-Host "build.ps1: '$args[0]' is not an option. See 'build.ps1 --help' for help."
+        Write-Output "build.ps1: '$args[0]' is not an option. See 'build.ps1 --help' for help."
     }
 }
 
